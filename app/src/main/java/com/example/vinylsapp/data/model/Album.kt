75c697471caeab800f5632df770acd546096f4cc
jp-hidalgo <jp.hidalgo@uniandes.edu.co>
2025-11-02 09:17:33 -0600

    Error correction

--- app/src/main/java/com/example/vinylsapp/data/model/Album.kt
+++ app/src/main/java/com/example/vinylsapp/data/model/Album.kt
@@ -54,10 +54,21 @@ data class Performer(
  * No incluye el campo id ya que es generado por el backend
  */
 data class CreateAlbumRequest(
+    @SerializedName("name")
     val name: String,
+    
+    @SerializedName("cover")
     val cover: String,
+    
+    @SerializedName("releaseDate")
     val releaseDate: String,
+    
+    @SerializedName("description")
     val description: String,
+    
+    @SerializedName("genre")
     val genre: String,
+    
+    @SerializedName("recordLabel")
     val recordLabel: String
 )
